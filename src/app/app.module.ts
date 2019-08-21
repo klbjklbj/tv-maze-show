aff7541c5b6328819aa6e5e66b09f1c6d31fbdcb
balaparameswari45 <52223968+balaparameswari45@users.noreply.github.com>
2019-08-20 20:09:33 -0700

    installed angular material and updated app module and app component html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyTvmazeshowComponent } from './my-tvmazeshow/my-tvmazeshow.component';
 import { MytvshowService } from './mytvshow/mytvshow.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatToolbarModule } from '@angular/material';
 
 
 @NgModule({
@@ -15,7 +17,14 @@ import { MytvshowService } from './mytvshow/mytvshow.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [MytvshowService],
   bootstrap: [AppComponent]
